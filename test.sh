e352045e62442b2c07eb7b26b6361b64aa2a49a4
William J. Schilp, PhD <william.schilp@ansys.com>
2023-11-14 14:45:19 -0500

    fix mklink to deal with winders vs linux backslashes

--- test.sh
+++ test.sh
@@ -1,16 +1,10 @@
 #! /usr/bin/env bash
 
-emacsExe=`find /c/Program\ Files/Emacs/ -name "emacs.exe"`
-echo "emacsExe = $emacsExe"
-echo "crap"
-if [ -z "$emacsExe" ]; then
-    echo "No winders emacs found"
-else
-    echo "winders emacs found"
-    emacsDir="$(dirname "$emacsExe")"
-    echo "more crap"
-    echo "emacsDir = $emacsDir"
-    echo "even more crap"
-    #export PATH=$PATH:$emacsDir
-fi
-#echo "PATH=$PATH"
+testStr="./Twin/foo"
+echo "testStr = $testStr"
+backSlash="\\\\"
+echo "backSlash = $backSlash"
+forwardSlash="/"
+echo "forwardSlash = $forwardSlash"
+echo ${testStr//\//\\}
+
